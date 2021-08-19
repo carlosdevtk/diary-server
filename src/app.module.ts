4e8151a9a7deeed223930b9e5851f1de0d087531
carlosdevtk <carlostkdev@gmail.com>
2021-08-18 21:48:01 -0300

    chore: Added the development db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: '/tmp/db.sqlite',
+          synchronize: true,
+          entities: [],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
