cf19633d6cb1f690c9d916296c2a841714f3cd5e
carlosdevtk <carlostkdev@gmail.com>
2021-08-20 22:41:34 -0300

    chore: Added the cookie-session to default connection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   (app as any).set('etag', false);
+  app.use(cookieSession({ keys: ['algumaChaveSecreta'] }));
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
