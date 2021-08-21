a8f82cd6564a31a0855c0fdbf1539c5cc536543d
carlosdevtk <carlostkdev@gmail.com>
2021-08-20 21:44:33 -0300

    refactor: Using the auth service to handle auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('/api')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
   @Post('/register')
   @HttpCode(201)
   registerUser(@Body() user: CreateUserDto) {
-    return this.usersService.createUser(user);
+    return this.authService.registerUser(user);
   }
 }
