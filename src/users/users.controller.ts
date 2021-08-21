21e0c005444a95d3fbbb6953071df658acb1daab
carlosdevtk <carlostkdev@gmail.com>
2021-08-20 22:43:11 -0300

    feat: Added guards and the logout route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/guards/Auth.guard';
+import { GuestGuard } from 'src/guards/Guest.guard';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('/api')
+@Serialize(UserDto)
 export class UsersController {
   constructor(
     private usersService: UsersService,
@@ -11,14 +24,25 @@ export class UsersController {
   ) {}
 
   @Post('/auth/register')
+  @UseGuards(GuestGuard)
   @HttpCode(201)
   registerUser(@Body() user: CreateUserDto) {
     return this.authService.registerUser(user);
   }
 
   @Post('/auth/login')
+  @UseGuards(GuestGuard)
   @HttpCode(200)
-  loginUser(@Body() dto: CreateUserDto) {
-    return this.authService.loginUser(dto);
+  async loginUser(@Body() dto: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.loginUser(dto);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/auth/logout')
+  @UseGuards(AuthGuard)
+  @HttpCode(204)
+  logoutUser(@Session() session: any) {
+    session.userId = null;
   }
 }
