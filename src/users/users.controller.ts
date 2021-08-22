384fa0847e0f42c0657aaefffdedd6c80ffe79ea
carlosdevtk <carlostkdev@gmail.com>
2021-08-22 17:14:11 -0300

    feat: Add patch route to update user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   HttpCode,
   Param,
+  Patch,
   Post,
   Session,
   UseGuards,
@@ -15,6 +16,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
@@ -47,7 +49,6 @@ export class UsersController {
   async loginUser(@Body() dto: CreateUserDto, @Session() session: any) {
     const user = await this.authService.loginUser(dto);
     session.userId = user.id;
-    console.log(session.userId);
     return user;
   }
 
@@ -68,5 +69,16 @@ export class UsersController {
   ) {
     await this.usersService.deleteUser(parseInt(id), currentUser);
     session.userId = null;
+  }
+
+  @Patch('/user/:id')
+  @UseGuards(AuthGuard)
+  @HttpCode(201)
+  updateUser(
+    @Param('id') id: string,
+    @CurrentUser() user: User,
+    @Body() attrs: UpdateUserDto,
+  ) {
+    return this.usersService.updateUser(parseInt(id), user, attrs);
   }
 }
