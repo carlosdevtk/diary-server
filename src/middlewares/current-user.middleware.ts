0c1aaa0df1a28b96d937241408d3a4acb8c9bd44
carlosdevtk <carlostkdev@gmail.com>
2021-08-22 16:37:11 -0300

    refactor: Changed the userId to actual user id

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -19,7 +19,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
   async use(request: Request, response: Response, next: NextFunction) {
     const { userId } = request.session || {};
     if (userId) {
-      const [user] = await this.usersService.findByUsername(userId);
+      const user = await this.usersService.findById(userId);
       request.currentUser = user;
     }
     next();
