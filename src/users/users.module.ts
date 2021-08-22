e855a55f503f42fe3b41b7f866438b2f4d75edc8
carlosdevtk <carlostkdev@gmail.com>
2021-08-22 14:50:51 -0300

    chore: Added current user middleware to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CurrentUserMiddleware } from 'src/middlewares/current-user.middleware';
 import { AuthService } from './auth.service';
 import { UsersController } from './users.controller';
 import { User } from './users.entity';
@@ -10,4 +11,8 @@ import { UsersService } from './users.service';
   controllers: [UsersController],
   providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
