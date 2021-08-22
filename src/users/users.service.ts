7fdd1942e7366643d79ed3a5af8c52484876a016
carlosdevtk <carlostkdev@gmail.com>
2021-08-22 16:39:06 -0300

    feat: Added the delete and update methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -7,13 +11,20 @@ import { User } from './users.entity';
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private usersRepo: Repository<User>) {}
-  async createUser(dto: CreateUserDto) {
+  async createUser(username: string, password: string) {
     const user = this.usersRepo.create({
-      username: dto.username,
-      password: dto.password,
+      username,
+      password,
     });
 
     return this.usersRepo.save(user);
+  }
+
+  findById(id: number) {
+    if (!id) {
+      return null;
+    }
+    return this.usersRepo.findOne(id);
   }
 
   findByUsername(username: string) {
@@ -21,5 +32,31 @@ export class UsersService {
       return null;
     }
     return this.usersRepo.find({ username });
+  }
+
+  async updateUser(id: number, currentUser: User, attrs: Partial<User>) {
+    const user = await this.findById(id);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+    if (currentUser.id !== user.id) {
+      throw new BadRequestException(
+        'Você não pode alterar dados de outro usuário >:(',
+      );
+    }
+    Object.assign(user, attrs);
+    return this.usersRepo.save(user);
+  }
+
+  async deleteUser(id: number, currentUser: User) {
+    if (currentUser.id !== id) {
+      throw new BadRequestException('Você não pode deletar outro usuário >:(');
+    }
+    const user = await this.findById(id);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return this.usersRepo.remove(user);
   }
 }
