8a82e0adae639584c05c5dff4cd75d1938bfdeda
carlosdevtk <carlostkdev@gmail.com>
2021-08-22 16:36:17 -0300

    fix: Added global pipes to handle the validations

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
@@ -7,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   (app as any).set('etag', false);
   app.use(cookieSession({ keys: ['algumaChaveSecreta'] }));
+  app.useGlobalPipes(new ValidationPipe());
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
