745c86420602175ad2dc677b8b340674600456a4
carlosdevtk <carlostkdev@gmail.com>
2021-08-23 20:53:42 -0300

    feat: Created the find user public notes method

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -5,13 +5,17 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
+import { UsersService } from 'src/users/users.service';
 import { Repository } from 'typeorm';
 import { CreateNoteDto } from './dtos/create-note.dto';
 import { Note } from './notes.entity';
 
 @Injectable()
 export class NotesService {
-  constructor(@InjectRepository(Note) private notesRepo: Repository<Note>) {}
+  constructor(
+    @InjectRepository(Note) private notesRepo: Repository<Note>,
+    private usersService: UsersService,
+  ) {}
 
   async createNote(dto: CreateNoteDto, user: User) {
     const note = this.notesRepo.create({ ...dto });
@@ -36,25 +40,48 @@ export class NotesService {
     return notes;
   }
 
+  async findUserPublicNotes(username: string) {
+    const [user] = await this.usersService.findByUsername(username);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const notes: Note[] = await this.notesRepo.find({
+      where: { user, isPublic: true },
+      relations: ['user'],
+    });
+
+    return notes;
+  }
+
   async findUserNote(username: string, id: number, currentUser: User) {
+    const [user] = await this.usersService.findByUsername(username);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
     if (!id) {
       return null;
     }
-    const note = await this.notesRepo.findOne(id);
+    const note = await this.notesRepo.findOne({
+      where: { id, user },
+      relations: ['user'],
+    });
 
     if (!note) {
       throw new NotFoundException('Esse diário não existe!');
     }
 
     if (!note.isPublic) {
-      if (currentUser.username !== username) {
+      if (currentUser !== user) {
         throw new BadRequestException(
           'Você não pode bisbilhotar um diário alheio',
         );
       }
       return note;
     } else {
-      if (currentUser.username === username) {
+      if (currentUser === user) {
         return note;
       }
       note.views += 1;
