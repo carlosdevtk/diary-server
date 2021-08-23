9b070bb8d1d1033f3f77742b26651f1d7e8ab93d
carlosdevtk <carlostkdev@gmail.com>
2021-08-23 20:52:57 -0300

    chore: Added user entity and service

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/users.entity';
+import { UsersService } from 'src/users/users.service';
 import { NotesController } from './notes.controller';
 import { Note } from './notes.entity';
 import { NotesService } from './notes.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Note])],
+  imports: [TypeOrmModule.forFeature([Note, User])],
   controllers: [NotesController],
-  providers: [NotesService],
+  providers: [NotesService, UsersService],
 })
 export class NotesModule {}
